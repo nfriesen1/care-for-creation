208300ee4920e33589664f399faff48685704bd6
Noah Friesen <frie1379@stthomas.edu>
2024-03-16 18:29:51 -0500

    more changes navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import './App.css';
-import AppRouter from './Routes';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "./App.css";
+import AppRouter from "./Routes";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Favorite, Park } from "@mui/icons-material";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="/">Care for Creation</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">Features</Nav.Link>
-            <Nav.Link href="/events">Events</Nav.Link>
-            <Nav.Link href='/resources'>Resources</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-        <AppRouter/>
+        <Navbar className="navbar">
+          <Container>
+            <Navbar.Brand href="/">
+              <Favorite fontSize="large" style={{color: '#FF4033'}}/>
+              <Park fontSize="large" style={{color: '#3A887C'}}/>
+            </Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">Features</Nav.Link>
+              <Nav.Link href="/events">Events</Nav.Link>
+              <Nav.Link href="/resources">Resources</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <AppRouter />
       </header>
     </div>
   );
